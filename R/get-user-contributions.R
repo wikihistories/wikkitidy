2a9bd89131e80a512a3853a44ab6b4c86469a563
Michael Falk <35712723+michaelgfalk@users.noreply.github.com>
2023-02-13 15:57:52 +1100

    building out core functions

--- R/get-user-contributions.R
+++
@@ -1,9 +0,0 @@
-get_user_contributions <- function(user, limit=50, ...) {
-  params <- rlang::list2(
-    list="usercontribs",
-    ucuser=user,
-    uclimit=limit,
-    ...
-  )
-  resp <- wiki_action_request(!!!params)
-}
---
+++ tests/testthat/test-wiki-action-request.R
@@ -0,0 +1,14 @@
+test_that("MediaWiki Action API request receives a 200 response code and json body", {
+  resp <- wiki_action_request(list = "categorymembers", cmtitle = "Category:Australian_historians") %>% httr2::req_perform()
+  expect_equal(httr2::resp_status(resp), 200)
+  expect_equal(httr2::resp_content_type(resp), "application/json")
+})
+
+test_that("get_list_of generates a valid query string with data in the query attribute", {
+  resp <- wiki_action_request() %>%
+    get_list_of("usercontribs", ucuser="Michaelgfalk") %>%
+    httr2::req_perform()
+  expect_equal(httr2::resp_status(resp), 200)
+  data <- httr2::resp_body_json(resp)
+  expect_gt(length(data$query$usercontribs), 1)
+})
