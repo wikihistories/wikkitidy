9a1a9660cdce2052852944f1aad1e5e0fced338f
Michael Falk <35712723+michaelgfalk@users.noreply.github.com>
2023-02-27 15:47:53 +1100

    base request for core REST API + tests

--- tests/testthat/test-rest-api.R
+++ tests/testthat/test-rest-api.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("`wikimedia_rest_request` returns a 200 for a basic request", {
+  response <- wikimedia_rest_request() %>%
+    httr2::req_url_path_append("page", "html", "Charles%20Harpur") %>%
+    httr2::req_perform()
+  expect_equal(httr2::resp_status(response), 200)
+})
+
+test_that("`core_rest_request` returns a 200 for a basic request", {
+  response <- core_rest_request() %>%
+    httr2::req_url_path_append("page", "Charles%20Harpur", "html") %>%
+    httr2::req_perform()
+  expect_equal(httr2::resp_status(response), 200)
 })
