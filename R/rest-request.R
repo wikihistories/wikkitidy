9a58a28577542891555696b304baee8e18506560
Michael Falk <35712723+michaelgfalk@users.noreply.github.com>
2023-04-21 12:24:12 +1000

    refactoring REST APIs

--- R/rest-request.R
+++ R/rest-request.R
@@ -1,16 +1,21 @@
-#' Base request for [MediaWiki Core REST
-#' API](https://www.mediawiki.org/wiki/API:REST_API)
+#' Build a REST request
 #'
-#' The [MediaWiki Core REST API](https://www.mediawiki.org/wiki/API:REST_API) is
-#' the basic REST API available on all MediaWiki wikis.
+#' @description `core_request_request()` builds a request for the [MediaWiki
+#' Core REST API](https://www.mediawiki.org/wiki/API:REST_API) is the basic REST
+#' API available on all MediaWiki wikis.
+#'
+#' `wikimedia_rest_request()` builds a request for the [Wikimedia REST
+#' API](https://www.mediawiki.org/wiki/Wikimedia_REST_API) is an additional
+#' endpoint just for Wikipedia and other wikis managed by the Wikimedia
+#' Foundation
 #'
 #' @param ... <[`dynamic-dots`][rlang::dyn-dots]> Components to add to the URL.
-#'  These are passed to [httr2::req_url_path_append]. If you pass named arguments,
-#'  the names will be dropped.
+#'   Unnamed arguments are added to the path of the request, while named
+#'   arguments are added as query parameters. See above.
 #' @param language The two-letter language code for the Wikipedia edition
 #'
-#' @return A `core_rest_request`, an S3 vector that subclasses `httr2_request`
-#'   (see [httr2::request])
+#' @return A `core/rest` or `wikimedia/rest` object, an S3 vector that
+#'   subclasses `httr2_request` (see [httr2::request])
 #'
 #' @export
 #'
@@ -18,54 +23,38 @@
 #' # Get the html of the 'Earth' article on English Wikipedia
 #' response <- core_rest_request("page", "Earth", "html") %>%
 #'   httr2::req_perform()
-core_rest_request <- function(..., language = "en") {
-  dots <- rlang::list2(...)
-  if (length(dots) == 0) {
-    rlang::abort("Invalid request: you have not provided any URL components")
-  }
-  request <- rlang::inject(
-    httr2::request(
-      glue::glue("https://{language}.wikipedia.org/w/rest.php/v1")
-    ) %>%
-      wikkitidy_user_agent() %>%
-      httr2::req_url_path_append(!!!dots)
-  )
-  class(request) <- c("core_rest_request", class(request))
-  request
-}
-
-#' Base request for [Wikimedia REST API](https://www.mediawiki.org/wiki/Wikimedia_REST_API)
 #'
-#' The [Wikimedia REST API](https://www.mediawiki.org/wiki/Wikimedia_REST_API) is one of
-#' the three main APIs for WikiPedia and its sister projects. This particular API
-#' is unique because its results are cached around the world can be downloaded with
-#' low latency. As a result, it is also the only one of the main APIs that requires
-#' users to throttle their usage.
-#'
-#' @inheritParams core_rest_request
-#'
-#' @return A `wikimedia_rest_request`, an S3 vector that subclasses
-#'  `httr2_request` (see [httr2::request])
-#' @export
-#'
-#' @examples
-#' # Get the html of the 'Earth' article
 #' response <- wikimedia_rest_request("page", "html", "Earth") %>%
 #'   httr2::req_perform()
-wikimedia_rest_request <- function(..., language = "en") {
-  dots <- rlang::list2(...)
-  if (length(dots) == 0) {
-    rlang::abort("Invalid request: you have not provided any URL components")
-  }
-  rlang::inject(
-    request <- httr2::request(
-      glue::glue("https://{language}.wikipedia.org/api/rest_v1")
-    ) %>%
-      httr2::req_throttle(199 / 1) %>%
-      httr2::req_url_path_append(!!!dots) %>%
-      wikkitidy_user_agent()
-  )
-  class(request) <- c("wikimedia_rest_request", class(request))
+core_rest_request <- function(..., language = "en") {
+  request <- rest_request(..., endpoint = "w/rest.php/v1", language = language)
+  class(request) <- c("core", class(request))
   request
 }
 
+#' @rdname core_rest_request
+wikimedia_rest_request <- function(..., language = "en") {
+  request <- rest_request(..., endpoint = "api/rest_v1", language = language) %>%
+    httr2::req_throttle(199 / 1)
+  class(request) <- c("wikimedia", class(request))
+  request
+}
+
+rest_request <- function(..., endpoint = character(), language = "en") {
+  dots <- rlang::dots_list(..., .named=FALSE)
+  path_components <- dots[!rlang::have_name(dots)]
+  if (length(path_components) == 0) {
+    rlang::abort("Invalid request: you have not provided any URL components")
+  }
+  query_params <- dots[rlang::have_name(dots)]
+  url <- glue::glue("https://{language}.wikipedia.org/")
+  rlang::inject(
+    request <- httr2::request(url) %>%
+      wikkitidy_user_agent() %>%
+      httr2::req_url_path_append(!!!endpoint) %>%
+      httr2::req_url_path_append(!!!path_components) %>%
+      httr2::req_url_query(!!!query_params)
+  )
+  class(request) <- c("rest", class(request))
+  request
+}
